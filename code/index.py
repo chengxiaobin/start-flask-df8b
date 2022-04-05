0c137bc3adc1f89324d3ed56a1be217955b26a1c
root <1040251337@qq.com>
2022-04-05 18:07:47 +0800

    initial commit

--- code/index.py
+++ code/index.py
@@ -1,6 +1,62 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+import cv2
+import numpy as np
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'upload/'
+app.config['PROCESS_FOLDER'] = 'process/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def process_img(filename):
+  iop = os.path.join(app.config['PROCESS_FOLDER'],filename)
+  img = cv2.imread(iop)
+  hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+  lb = np.array([110,50,50])
+  ub = np.array([130,255,255])
+  mask = cv2.imRange(hsv,lb,ub)
+  eimg = cv2.bitwise_and(img,img,mask=mask) 
+  isp = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+  cv2.imwrite(isp,eimg,[int(cv2.IMWRITE_JPEG_QUALITY),100)
+
+def ris(ilp):
+  import base64
+  ims = ''
+  with open(ilp,'rb') as img_f
+    ims = img_f.read()
+    ims = base64.b64encode(ims)
+    ims64 = str(ims,'utf-8')
+  return ims64
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+@app.route('/uploader',methods=['GET','POST'])
+def uploader():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+		flash('no file part')
+		return render_template('upload.html')
+	f = request.files['file']
+        if f.filename == ''
+		flash('no selected file')
+		return render_template('upload.html')
+	if f and allowed_file(f.filename)
+		filename = secure_filename(f.filename)
+		imp = os.path.join(app.config['PROCESS_FOLDER'],filename)
+		f.save(imp)
+		process_img(filename)
+		irp = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+		ims = ris(irp)
+		os.remove(imp)
+		os.remove(irp)
+		return render_template('show.html',img_stream=ims)
+	return render_template('upload.html')
 
 
 @app.route('/')
